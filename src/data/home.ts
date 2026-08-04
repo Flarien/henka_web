4e4b5f85ba75b6e0cfa126d9de30646281fd7bbf
Flavia S. Briglia <fbrig87@gmail.com>
2026-08-04 17:46:16 -0300

    agrega nuevas secciones de Colecciones, Materiales y Metodo

--- src/data/home.ts
+++ src/data/home.ts
@@ -1,4 +1,4 @@
-export interface Colecciones {
+export interface Coleccion {
   title: string;
   description: string;
   image: string;
@@ -7,36 +7,28 @@ export interface Colecciones {
   href?: string;
 }
 
-export const categories: Colecciones[] = [
+export const categories: Coleccion[] = [
   {
-    title: "Remeras",
+    title: "Aikido",
     description:
-      "Diseños que resisten el entrenamiento más intenso sin perder color.",
-    image:
-      "https://lh3.googleusercontent.com/aida-public/AB6AXuD81yDpXP2F2GEVE3jSWzvfF6cIRRMTw2SRkOKE3e9qu-uexVBIhYlq834MUhbKAsvOsNMIPjJVlrWAKFRpSN_9aAHIMU8pDxm26ShIKpZ8jGwdTOTP1nlhX3JRQHblCgxyEGzJxSw6vVgfCxikWQLVoptfQcPDCxdcuevJWnlvqRnBjdPaG-llGl8sN7CyJv2Hq-wl4yAuk3_yhqpSQeVZ4q696h_69umUogocPS058diQgDXk4S40NCGxfZjIEktdJCjJNtMF3bK1",
-    badge: "Técnico & Algodón",
+      "Remeras en algodón oscuro con diseño claro, y en tela técnica clara con diseño oscuro para entrenar. También en toallas y tazas.",
+    image: "/images/coleccion-aikido.png",
     layout: "large",
-    href: "/productos/remeras"
+    href: "/colecciones/aikido",
   },
-
   {
-    title: "Accesorios",
-    description:
-      "Tazas, llaveros y productos personalizados de alta calidad.",
-    image:
-      "https://lh3.googleusercontent.com/aida-public/AB6AXuCYP28YKGHsSOLpL2NAwmaHwsdCkkDdO6cMcVfHueNRQUbxIWz1Nwq2Z4p2rykuRxcbbZG8Y56fKCunNNuD9jAKFPRNLsV2catNBzdE2_uS28xP5OaNEDQY_RZZlQFSaP5ou_UzqMuZ4D__AAzVXN3asDB-QlmJ3Ntw0zMVS_bqhHVXS1kw-P3zYP5OStOr7C3E2azlCXlAdHWEd6s1hk0JwogJVluDZAu9ZjfxRkobVImS5HJ5Nm7vGOrVXHGBIwnUoGCMNiJdJPrA",
+    title: "Argentina",
+    description: "Remeras, buzos, taza y mate con los colores de siempre.",
+    image: "/images/coleccion-argentina.png",
     layout: "small",
-    href: "/productos/accesorios"
+    href: "/colecciones/argentina",
   },
-
   {
-    title: "Buzos & Hoodies",
+    title: "Personalizados",
     description:
-      "Prendas de abrigo con estampas de gran formato que no se agrietan ni se despegan.",
-    image:
-      "https://lh3.googleusercontent.com/aida-public/AB6AXuBcHlXVoPLZwUIlTC0IkYhWXayHvbDilPQtb3iUaRvgeZYoN71xg7et8BpuzIYAcBpboU1Mo1ll_C80YTjnbvBAr6yfUCtQvjGtaeZwwBiYkH2kC3zaL9wAOCJphHuXtSkEreEIxV8YmaVRtzRSbQLg80IOTofAcreBqwbRe8fB6IThorUnmEfZYy0uk50VTcZi2MKHn6ZpG7dqqGcwz0t7iUvogEyGrmM9sOw14w9RAlY9qITz80bXc5m_gxNA38OicT2brGhxVkoJ",
+      "Contanos qué tenés en mente y lo convertimos en una pieza única, sin mínimos.",
+    image: "/images/coleccion-personalizados.jpg",
     layout: "wide",
-    href: "/productos/buzos"
-  }
+    href: "/colecciones/personalizados",
+  },
 ];
-
